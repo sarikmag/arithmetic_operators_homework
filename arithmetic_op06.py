7aa21d08106283018aca1a9d7e0a730efd359c91
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-20 10:21:55 +0500

    ads

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -3,7 +3,8 @@ number = 11
 #Divide the number by 2 and assign number.
 number = number / 2
 #Find the fraction of the number and assing to a variable called 'answer'.
-answer = number % (number//1)
+from math import floor
+answer = number % (floor(number))
 #Print the answer.
 print(answer)
 #Print the number.
